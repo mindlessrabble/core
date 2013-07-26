0124d49e711fbe59ee731d6d909248f6205481a7
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2013-07-27 02:36:14 +0530

    Added 'problematic' RequiresFacet annotation.

--- scaffold/faces/src/main/java/org/jboss/forge/addon/scaffold/faces/FacesScaffold.java
+++ scaffold/faces/src/main/java/org/jboss/forge/addon/scaffold/faces/FacesScaffold.java
@@ -3,12 +3,23 @@ package org.jboss.forge.addon.scaffold.faces;
 import java.util.List;
 
 import org.jboss.forge.addon.facets.AbstractFacet;
+import org.jboss.forge.addon.facets.constraints.RequiresFacet;
+import org.jboss.forge.addon.javaee.facets.CDIFacet;
+import org.jboss.forge.addon.javaee.facets.EJBFacet;
+import org.jboss.forge.addon.javaee.facets.PersistenceFacet;
 import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.facets.DependencyFacet;
 import org.jboss.forge.addon.resource.DirectoryResource;
 import org.jboss.forge.addon.resource.Resource;
 import org.jboss.forge.addon.scaffold.ScaffoldProvider;
 import org.jboss.forge.addon.ui.wizard.UIWizardStep;
 
+@RequiresFacet({
+    DependencyFacet.class,
+    PersistenceFacet.class,
+    EJBFacet.class,
+    CDIFacet.class
+})
 public class FacesScaffold extends AbstractFacet<Project> implements ScaffoldProvider {
 
 	@Override
